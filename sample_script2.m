82b93b4145544903d010ac27f77a0ec0f4332f32
Devin Kachan <devin.kachan@gmail.com>
2014-11-20 19:19:41 -0800

    ic4

--- sample_script2.m
+++ sample_script2.m
@@ -1,7 +1,7 @@
 p = SimParams();
 p.N_time_steps = 4000;
 p.display_interval = 1;
-p.show_mesh = true;
+p.show_mesh = false;
 p.show_figure = true;
 %Obstacles!
 o1 = LineObstacle([40 40], [40 60], 1, ObstacleTypes.DoubleVerticalLine);
@@ -9,8 +9,8 @@ o2 = LineObstacle([50 40], [80 40], 1, ObstacleTypes.DoubleHorizontalLine);
 o3 = LineObstacle([50 60], [80 60], 1, ObstacleTypes.DoubleHorizontalLine);
 o4 = CircleObstacle([20 25], 7);
 
-p.obstacles = [ LineObstacle([40 40], [40 60], 2, ObstacleTypes.DoubleVerticalLine)...
-    CircleObstacle([25 50], 4)];
+%p.obstacles = [ LineObstacle([40 40], [40 60], 2, ObstacleTypes.DoubleVerticalLine)...
+%    CircleObstacle([25 50], 4)];
 
 
 
